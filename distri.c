f2305153f679321c593b39c61570efb0ccb8b241
pr-juan-k <djjuankalexis18@gmail.com>
2025-04-21 23:21:50 -0300

    tarea realizada

--- distri.c
+++ distri.c
@@ -19,13 +19,17 @@ Nodo * crearTarea(char *describ,int duracion,int tareaID);
 void agregarNodo(Nodo **start,Nodo *nodo);
 Nodo *tareasRealizadas(Nodo **start,int inTarea);
 void mostrarLista(Nodo *start);
+void quitarElemento();
+void buscarPorID(Nodo *tareaR,Nodo *tareaP,int id);
+void buscarPorCadena(Nodo *tareaT,Nodo *tareaP,char * cadena);
 int main(){
     srand(time(NULL));
 
     Nodo * start = NULL;
     Nodo * start_Trealizadas = NULL;
     int seguir = 0,inDurac = 0,tareaID = 1000,realizada = 0;
-    char inDescr[20];
+    int id = 0,opcion = 0;
+    char inDescr[20],cadena[20];
     
         printf("\nDesea cargar tarea 1-si / 2-no:");
         scanf("%d",&seguir);
@@ -64,12 +68,39 @@ int main(){
     }
     
     puts("##################");
+    printf("\nTAREAS PENDIENTES");
     mostrarLista(start);
     printf("\n");
     puts("##################");
     printf("\nTAREAS REALIZADAS");
     mostrarLista(start_Trealizadas);
 
+    do
+    {
+        printf("\n1-por ID \n 2-por CADENA \n como desea buscar:");
+        scanf("%d",&opcion);
+        switch (opcion)
+        {
+        case 1:
+            printf("\nIngrese el numero por id a buscar:");
+            scanf("%d",&id);
+            buscarPorID(start,start_Trealizadas,id);
+            break;
+        
+        case 2:
+            printf("\nIngrese una cadena para buscar:");
+            scanf("%s",cadena);
+            buscarPorCadena(start,start_Trealizadas,cadena);
+            break;
+        }
+
+         printf("\nDesea seguir consultando 1-si / 2-no:");
+        scanf("%d",&seguir);
+    } while (seguir != 2);
+    
+    
+
+    
 
 
     return 0;
@@ -98,23 +129,89 @@ void agregarNodo(Nodo **start,Nodo *nodo){
 }
 Nodo *tareasRealizadas(Nodo **start,int inTarea){
     Nodo *aux = * start;
+    Nodo *anteriro = NULL;
+
     while (aux != NULL && aux->T.TareaID != inTarea)
     {
+        anteriro = aux;
         aux = aux->Siguiente;
     }
+    if (anteriro == NULL)
+    {
+        *start = aux->Siguiente;
+    }else{
+        anteriro->Siguiente = aux->Siguiente;
+    }
+    aux->Siguiente = NULL;
     return aux;
     
 }
 void mostrarLista(Nodo *start){
+    int posicion = 1;
     Nodo *aux = start;
     while (aux != NULL)
     {
-        
+        printf("\n%d-posicion",posicion);
         printf("\n\nTarea id: %d",aux->T.TareaID);
         printf("\nDescripcion: %s",aux->T.Descripcion);
         printf("\nDuracion:%d",aux->T.Duracion);
         aux = aux->Siguiente;
+        posicion++;
     }
     
 }
+void buscarPorID(Nodo *tareaR,Nodo *tareaP,int id){
+    Nodo *auxT = tareaR;
+    Nodo *auxP = tareaP;
 
+    while (auxT != NULL)
+    {
+        if (auxT->T.TareaID == id)
+        {
+            printf("\nTarea realizada");
+            printf("ID: %d\nDescripcion: %s\nDuracion: %d\n", auxT->T.TareaID, auxT->T.Descripcion, auxT->T.Duracion);
+        }
+        
+        auxT = auxT->Siguiente;
+    }
+    while (auxP != NULL)
+    {
+        if (auxP->T.TareaID != id)
+        {
+            printf("\nTarean pendiente");
+            printf("ID: %d\nDescripcion: %s\nDuracion: %d\n", auxP->T.TareaID, auxP->T.Descripcion, auxP->T.Duracion);
+        }
+        auxP = auxP->Siguiente;
+    }
+    
+
+
+    
+    
+}
+
+void buscarPorCadena(Nodo *tareaT,Nodo *tareaP,char * cadena){
+    Nodo *auxT = tareaT;
+    Nodo *auxP = tareaP;
+    while  (auxT != NULL)
+    {
+        if (strcmp(auxT->T.Descripcion,cadena) == 0)
+        {
+            printf("\nTarea realizada");
+            printf("ID: %d\nDescripcion: %s\nDuracion: %d\n", auxT->T.TareaID, auxT->T.Descripcion, auxT->T.Duracion);
+        }
+        auxT = auxT->Siguiente;
+        
+    }
+    while (auxP != NULL)
+    {
+        if (strcmp(auxP->T.Descripcion,cadena) == 0)
+        {
+            printf("\nTarean pendiente");
+            printf("ID: %d\nDescripcion: %s\nDuracion: %d\n", auxP->T.TareaID, auxP->T.Descripcion, auxP->T.Duracion);
+        }
+        auxP =auxP->Siguiente;
+    }
+    
+    
+}
